39f0182abf8fbbb3609ce0b35d728345cee9ab2e
Jo L <joseph@josephchilin.com>
2020-04-16 13:39:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,30 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_sort = {}
+    data.each do |color_gender_lives_key, value|
+      value.each do |stats_key, value2|
+        value2.each do |name|
+          if pigeon_sort[name] == nil
+            pigeon_sort[name] = {}
+          end
+          
+          
+          
+          
+          
+          
+          
+          
+        end
+        
+        
+        
+      end
+      
+      
+    end
+  
+  pigeon_sort
 end
